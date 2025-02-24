e2a7d963a07494a28260acb3b627de56d2d5b31e
Chaz Schlarp <schlarpc@gmail.com>
2025-02-24 06:55:21 +0000

    Format

--- flake.nix
+++ flake.nix
@@ -9,18 +9,39 @@
     };
   };
 
- outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.simpleFlake {
-       inherit self nixpkgs;
-       name = "haynow";
-       config = { allowUnfree = true; segger-jlink.acceptLicense = true; permittedInsecurePackages = [
-                "segger-jlink-qt4-810"
-              ];
-              }; 
-       shell = { pkgs }: pkgs.mkShell { 
-       
-       SDK_ROOT = "${pkgs.nrf5-sdk}/share/nRF5_SDK";
-       GNU_INSTALL_ROOT = "${pkgs.gcc-arm-embedded-13}";
-       buildInputs = with pkgs; [ nrf-command-line-tools  gcc-arm-embedded-13 (python3.withPackages (ps: with ps; [intelhex cryptography])) ]; };
+      inherit self nixpkgs;
+      name = "haynow";
+      config = {
+        allowUnfree = true;
+        segger-jlink.acceptLicense = true;
+        permittedInsecurePackages = [
+          "segger-jlink-qt4-810"
+        ];
+      };
+      shell =
+        { pkgs }:
+        pkgs.mkShell {
+
+          SDK_ROOT = "${pkgs.nrf5-sdk}/share/nRF5_SDK";
+          GNU_INSTALL_ROOT = "${pkgs.gcc-arm-embedded-13}";
+          buildInputs = with pkgs; [
+            nrf-command-line-tools
+            gcc-arm-embedded-13
+            (python3.withPackages (
+              ps: with ps; [
+                intelhex
+                cryptography
+              ]
+            ))
+          ];
+        };
     };
 }
